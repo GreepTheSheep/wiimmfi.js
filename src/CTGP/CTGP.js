59f67a6d46478489b6ee65541d5e824eaadaa23a
Matthieu Dehon <89matt89.md@gmail.com>
2021-02-04 10:56:09 +0100

    Add caching system with events

--- src/CTGP/CTGP.js
+++ src/CTGP/CTGP.js
@@ -1,13 +1,34 @@
 const fetch = require('node-fetch')
 const url = require('./httpOptions.js')
+const {EventEmitter} = require('events')
 
-class CTGP {
-    constructor(){
+class CTGP extends EventEmitter{
+    /**
+     * Leaderboard for CTGP-R (https://chadsoft.co.uk/time-trials/)
+     * @param {object} options Options for CTGP-R Leaderboard
+     * @param {boolean} options.cache Default: true, enables caching at start
+     * @param {boolean} options.autoCache Default: true, refresh caches, this uses setInterval, this works if options.cache is true
+     * @param {number} options.autoCacheRefresh Default: 2, The delay in minutes to refresh the cache, if options.autoCache is true
+     */
+    constructor(options = {
+        cache: true,
+        autoCache: true,
+        autoCacheRefresh: 2
+    }){
+        super()
+        this.options = options
+
+
         this.url = `${url.protocol}://${url.host}`
+        this.cache = {}
+
+        if (options.cache) this._startCache()
+        if (options.cache && options.autoCache) this._autoCache(options.autoCacheRefresh)
     }
 
     /**
      * Simplify links return
+     * @returns {object} All links with their key
      * @private
      */
     async _getLinks(){
@@ -26,6 +47,7 @@ class CTGP {
 
     /**
      * Gets data from this.url
+     * @returns {object} RAW JSON for this API
      * @private
      */
     async _getData(link = "index"){
@@ -34,8 +56,50 @@ class CTGP {
             var links = await this._getLinks()
             var seletedLink = links[link]
             if (!seletedLink) throw 'Bad link name, check index'
-            else return await fetch(seletedLink).then(r=>r.text()).then(text=>JSON.parse(text.substring(1)))
+            if (!this.cache[link]){
+                if (this.options.cache) {
+                    this.cache[link] = await fetch(seletedLink).then(r=>r.text()).then(text=>JSON.parse(text.substring(1)))
+                    return this.cache[link]
+                }
+                else return await fetch(seletedLink).then(r=>r.text()).then(text=>JSON.parse(text.substring(1)))
+            }
+            else return this.cache[link]
         }
+    }
+
+    /**
+     * Start caching all main links
+     * @private
+     */
+    async _startCache(){
+        var links = await this._getLinks()
+        Object.entries(links).forEach(async entry => {
+            const [key] = entry;
+            this.emit('debug', '[Cache] Starting caching for ' + key)
+            const data = await this._getData(key)
+            this.cache[key] = data
+            this.emit('cache-update', key, data)
+            this.emit('debug', '[Cache] Caching successful for ' + key)
+        });
+    }
+
+    /**
+     * Auto-caching all main links method
+     * @param {number} refreshTimeMin The delay to refresh in minutes
+     * @private 
+     */
+    async _autoCache(refreshTimeMin){
+        var links = await this._getLinks()
+        Object.entries(links).forEach(async entry => {
+            const [key] = entry;
+            setInterval(async ()=>{
+                this.emit('debug', '[Cache] Starting caching for ' + key)
+                const data = await this._getData(key)
+                this.cache[key] = data
+                this.emit('cache-update', key, data)
+                this.emit('debug', '[Cache] Caching successful for ' + key)
+            }, refreshTimeMin*60*1000)
+        });
     }
 }
 
--- test/ctgp.js
+++ test/ctgp.js
@@ -1,9 +1,15 @@
 const Wiimmfi = require('../')
 const CTGP = new Wiimmfi.CTGP()
 
+// CTGP.on('debug', msg=>{
+//     console.log(msg)
+// })
+
+CTGP.on('cache-update', (key, data)=>{
+    console.log(key, data)
+})
+
 async function run(){
     console.log(CTGP.url,await CTGP._getLinks())
-    const players = await CTGP._getData("players")
-    console.log(players.filter(p=>p.miiName == 'Greep'))
 }
 run()
