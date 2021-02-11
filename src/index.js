04e55ce8f15eaa18eeee20f09b9bf7f7e58cb845
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2021-02-11 11:25:05 +0100

    update test command

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 module.exports = {
+    // Classes
     MKWii: require('./wiimmfi/mkwii/MKWii.js'),
     CTGPOnline : require('./CTGP/CTGPO/CTGP-online.js'),
-    CTGP : require('./CTGP/CTGP.js')
+    CTGP : require('./CTGP/CTGP.js'),
+
+    // single methods
+    getError : require('./wiimmfi/error-codes/error-codes.js')
 }
